678f6f5d9231c11ff8ee747be1aee3c72f63715b
hgf <heugangfu@ict.ac.cn>
2023-07-07 16:36:00 +0800

    Update: Clip ray serve test

--- test/clip/test_ray_serve.py
+++ test/clip/test_ray_serve.py
@@ -1,29 +1,36 @@
+import json
 import unittest
 from pathlib import Path
 
+import httpx
 import ray
-from ray import serve
 
 from zoo.config import data_dir
-from zoo.backends.pytorch.cn_clip.ray_serve import clip
 
 
 class ClipServeTest(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        cls.handle = serve.run(clip)
+        cls.host = 'http://localhost:8000/clip'
 
     def test_clip_ray_serve_text(self):
-        embedding = ray.get(self.handle.text_features.remote(text=["这是一只皮卡丘", "hello"]))
-        print(embedding)
-        self.assertEqual(embedding.shape[1], 1024)
+        data = ["这是一只皮卡丘", "hello"]
+        # headers = {"Content-Type": "application/json"}
+        response = httpx.post(self.host + '/text', json=data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(data), len(response.json()))
 
     def test_clip_ray_serve_image(self):
-        with open(Path(data_dir) / 'pokemon.jpeg', 'rb') as f:
-            embedding = ray.get(self.handle.image_features.remote(image=[f.read()]))
-            print(embedding)
-            self.assertEqual(embedding.shape[1], 1024)
+        files = []
+        file_names = ['pokemon.jpeg']
+        for file_name in file_names:
+            files.append(('image', (file_name, open(Path(data_dir) / file_name, 'rb'), 'image/png')))
+
+        # 发送POST请求
+        response = httpx.post(self.host + '/image', files=files)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(files), len(response.json()))
 
 
 if __name__ == '__main__':
--- test/clip/test_ray_serve_client.py
+++ test/clip/test_ray_serve_client.py
@@ -3,5 +3,5 @@ from zoo.config import data_dir
 from pathlib import Path
 
 img_byte = [open(Path(data_dir) / 'pokemon.jpeg', 'rb').read(), open(Path(data_dir) / 'pokemon.jpeg', 'rb').read()]
-response = requests.post("http://localhost:8265/clip/image", data=img_byte)
+response = requests.post("http://localhost:8265/image", data=img_byte)
 print(response.text)
